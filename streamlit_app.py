0f3304b7acaafccaf7184fca91906d8e3469d6d6
RAlekhya09 <alekhyarayalla2003@gmail.com>
2025-12-29 15:54:46 +0530

    Display smoothiefroot response as JSON

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,6 +43,6 @@ if ingredients_list:
 # New section to display smoothiefroot information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
 
 
